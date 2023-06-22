9b85616e5977ebe7d46102764f18e98ff982298c
ajjiad <jslworld365@gmail.com>
2023-06-22 14:29:06 +0600

    added the new code-file: (new-lines/line-brek)

---
+++ C-new-lines/c_new_lines.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("Hey, I am Abdullah Al Jiad\nStudent of Daffodil International University\nDepartment of Computer Science and Engineering\n");
+    return 0;
+}
