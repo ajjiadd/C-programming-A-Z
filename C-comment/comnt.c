ccca8dee2903e20bc3fe4f0442a38fd641aeed1d
ajjiad <jslworld365@gmail.com>
2023-06-22 15:21:50 +0600

    new added file is: C-switch/C-variabls/If-else

---
+++ C-comment/comnt.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("Hey Buddies!\n how are you?\n");  //This is C-comment
+    return 0;
+}
---
+++ C-variables/intagers.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int X = 10, Y = 20, sum;
+    sum = X + Y;
+
+    printf("The results is : %d\n",sum);
+    return 0;
+}
