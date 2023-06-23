906a5eec27e3594b4055751f4cfadb75b4a55e60
ajjiad <jslworld365@gmail.com>
2023-06-23 14:52:15 +0600

    modified: loop_func

--- C Functions ALL Example/loop_func.c
+++ C Functions ALL Example/loop_func.c
@@ -1,11 +1,12 @@
-void myFunction(int myNumbers[5]) {
-  for (int i = 0; i < 5; i++) {
-    printf("%d\n", myNumbers[i]);
-  }
+#include<stdio.h>
+void loopary(int ary[10]){
+    int i;
+    for(i = 0; i < 10; i++){
+        printf("%d->",ary[i]);
+    }
 }
-
-int main() {
-  int myNumbers[5] = {10, 20, 30, 40, 50};
-  myFunction(myNumbers);
-  return 0;
+int main(){
+    int  ary[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    loopary(ary);
+    return 0;
 }
